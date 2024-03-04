d4d4a0b1f4143243c21b92656fa23dfdee2dc8d2
NikaKolin <138161406+NikaKolin@users.noreply.github.com>
2024-03-04 15:46:58 -0500

    Update MainClass.java

--- CP317/Cp317/MainClass.java
+++ CP317/Cp317/MainClass.java
@@ -5,7 +5,7 @@ package CP317;
 public class MainClass {
   public static void main(String[] args) {
     System.err.println("my name is ama");
-    
+    System.out.println("Testing this irght now, let me know if it works - Nika")
   }
 
   
